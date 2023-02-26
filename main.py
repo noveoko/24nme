a36225ec54547ced9410e245378cab7471a622bd
Marcin Kraszewski <visitwarsaw@gmail.com>
2023-02-26 19:58:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
-from flask import Flask, render_template
+def main():
+    logging.info('Starting the application')
+    # your code here
+    from flask import Flask, render_template
 
-app = Flask(__name__)
+    app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+    @app.route('/')
+    def home():
+        return render_template('index.html')
+
+    if __name__ == '__main__':
+        app.run()
+
+
+
+    import logging
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s %(levelname)s: %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+    logging.info('Finished running the application')
 
 if __name__ == '__main__':
-    app.run()
+    main()
