9bfce72847ba80c7fc0bc395f982134c10dd47fd
Marcin Kraszewski <visitwarsaw@gmal.com>
2023-02-27 20:25:35 +0100

    updated file

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,7 +6,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from main import app
 
 def test_home_page():
-    client = app.app.test_client()
+    client = app.test_client()
     response = client.get('/')
     assert response.status_code == 200
     assert b'Hello, World!' in response.data
