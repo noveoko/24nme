ac3256786758fcc4712efd6928234ed8440eca80
Marcin Kraszewski <visitwarsaw@gmal.com>
2023-02-27 08:56:17 +0100

    attepmt to fix

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,7 +3,7 @@ import os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-import app
+from main import app
 
 def test_home_page():
     client = app.app.test_client()
