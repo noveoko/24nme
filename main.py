8d9f012c67b10ceab70bc32c8a116e9e0d27c1c6
Marcin Kraszewski <visitwarsaw@gmal.com>
2023-03-02 20:36:34 +0100

    added form validation

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template, request
-import logging
-from predict import random_country
+from flask import Flask, render_template, request, redirect
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField
+from wtforms.validators import DataRequired, NumberRange
+from predict import random_country
+import logging
+import os
 
 logging.basicConfig(
     level=logging.INFO,
@@ -11,21 +15,26 @@ logging.basicConfig(
 )
 
 app = Flask(__name__)
-limiter = Limiter(
-    get_remote_address,
-    app=app,
-    default_limits=["200 per day", "50 per hour"],
-    storage_uri="memory://",
-)
+app.config['SECRET_KEY'] = os.environ.get('FLASK_SECRET_KEY')
 
-@limiter.limit("1/second", override_defaults=False)
+limiter = Limiter(key_func=get_remote_address)
+
+class PredictForm(FlaskForm):
+    person_name = StringField('Person Name', validators=[DataRequired()])
+    year = IntegerField('Year (birth/death/alive during)', validators=[DataRequired(), NumberRange(min=1600, max=2022)])
+
 @app.route('/', methods=['GET', 'POST'])
+@limiter.limit("1/second")
 def home() -> str:
     logging.info('Rendering index.html')
-    return render_template('index.html')
+    form = PredictForm()
+    if form.validate_on_submit():
+        # Handle form submission here
+        return redirect('/predict')
+    return render_template('index.html', form=form)
 
-@limiter.limit("1/second", override_defaults=False)
 @app.route('/predict', methods=['POST'])
+@limiter.limit("1/second")
 def predict() -> str:
     person_name: str = request.form['person_name']
     year: int = int(request.form['year'])
