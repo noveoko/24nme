39b741a78b8d3d9bd47c88bdcddf0f1720ff67ec
Marcin Kraszewski <visitwarsaw@gmal.com>
2023-03-03 10:13:33 +0100

    fixed test

--- tests/test_app.py
+++ tests/test_app.py
@@ -11,4 +11,4 @@ def test_home_page():
     client = app.test_client()
     response = client.get('/')
     assert response.status_code == 200
-    assert b'Hello, World!' in response.data
+    assert b'https://cdn.jsdelivr.net/npm/bootstrap@' in response.data
